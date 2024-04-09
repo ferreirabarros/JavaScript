d55af175d33b42a8eca68851092c2b91d8cf5199
ferreirabarros <alexandreferreirabarros@hotmail.com>
2024-04-08 21:19:46 -0300

    tipos primitivos

---
+++ tipos-primitivos.js
@@ -0,0 +1,38 @@
+// string = conjusto de caracteres (entre "" ou entre '' ou entre `` ou entre )
+let nome = "Xandy";
+
+//typeof mostra o tipo primitivo da variaveis
+console.log(typeof nome, nome);
+
+
+
+// number = numeros (simplimente somente numeros)
+let num = 12;
+
+//typeof mostra o tipo primitivo da variaveis
+console.log(typeof num, num);
+
+
+
+
+// boolean = true ou false (verdadeiro ou falso)
+let vf = true;
+
+//typeof mostra o tipo primitivo da variaveis
+console.log(typeof vf, vf);
+
+
+
+// null/object = nulo 
+let nulo = null;
+
+//typeof mostra o tipo primitivo da variaveis
+console.log(typeof nulo, nulo);
+
+
+
+// underfned = indefinida
+let indef;
+
+//typeof mostra o tipo primitivo da variaveis
+console.log(typeof indef, indef);

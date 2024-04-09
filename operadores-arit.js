65f18429ed8b5d2c2f4329eaf12803da187de751
ferreirabarros <alexandreferreirabarros@hotmail.com>
2024-04-08 22:15:12 -0300

    operadores aritimedicos

---
+++ operadores-arit.js
@@ -0,0 +1,36 @@
+var valor1 = 1;
+var valor2 = 2;
+
+var nome = "xandy";
+var sobrenome = "Ferreira";
+
+console.log(valor1 + valor2);
+// + vai somas os valores
+console.log(nome + sobrenome);
+// + vai juntas os valores
+
+
+
+
+var valor1 = 3;
+var valor2 = 2;
+
+
+console.log(valor2 - valor1);
+// - vai subtrair os valores
+
+
+var valor1 = 3;
+var valor2 = 2;
+
+
+console.log(valor1 * valor2);
+// * vai multiplicar os valores
+
+
+var valor1 = 5;
+var valor2 = 2;
+
+
+console.log(valor1 / valor2);
+// / vai dividir os valores

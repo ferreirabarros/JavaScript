9ccbb0d5159b85e46be35d75c5a0bd54c868236c
ferreirabarros <alexandreferreirabarros@hotmail.com>
2024-04-08 22:34:36 -0300

    operadores aritimeticos, atrinuição e comparação

---
+++ op-atribuicao.js
@@ -0,0 +1,7 @@
+var valor = 3;
+
+valor += 2;
+
+// o = atribui ao valor da esquerda o valor da sua direita
+
+console.log(valor);
---
+++ op-comparacao.js
@@ -0,0 +1,35 @@
+var idade = 25;
+
+console.log(idade > 10);
+// > maior que
+
+
+var idade = 25;
+
+console.log(idade < 10);
+// > menor que
+
+
+var idade = 25;
+
+console.log(idade == 10);
+// == iqual a
+
+var idade = 25;
+
+console.log(idade == "25");
+// == iqual a mesmo sento tipos diferentes
+
+
+var idade = 25;
+
+console.log(idade === "25");
+// === exatamente iqual a
+
+
+
+var idade = 25;
+
+console.log(idade != 24);
+// != diferente de (mesmo sendo do mesmo tipo)
+
